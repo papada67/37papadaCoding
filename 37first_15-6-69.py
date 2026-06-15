442fb9b22153c418c37262772ec9d1e60c316fde
papada67 <27087@bbw.ac.th>
2026-06-15 09:45:50 +0700

    โปรมแกรมเลขคู่เลขคี่
    ได้รู้วิธีใช้โปรมแกรมเลขคู่ เลขคี่ I P O

---
+++ 37first_15-6-69.py
@@ -0,0 +1,9 @@
+number = int(input("Enter you number: "))
+
+if number %2 == 0:
+    print("เลขคู่")
+else:
+    print("เลขคี่")
+print("โปรมแกรมเลขคู่หรือเลขคี่")
+print("by first 4/4")
+print("thank you")
