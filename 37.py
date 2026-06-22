454aca6aa65363f838c10e36b860480b64f848bf
papada67 <27087@bbw.ac.th>
2026-06-22 09:40:49 +0700

    โปรแกรมคำนวณคะแนนรวมและคะแนนเฉลี่ย 2
    เรียนรู้เกี่ยวกับเขียนโค้ดคะแนนรวมและเฉลี่ยคะแนน ครูสอนเข้าใจมากๆ ชอบที่คุณครูเดินดูนักเรียนค่ะ

--- 37.py
+++ 37.py
@@ -1,4 +1,20 @@
-name = input("คุณชื่ออะไร? ")
-age = int(input("อายุเท่าไหร่? "))
-print("สวัสดี" , name)
-print("อีก 1 ปี คุณจะอายุ" , age + 1, "ปี")
+print("โปรแกรมคำนวณคะแนนรวม\n")
+
+point_math = int(input("คะแนนวิชาคณิตศาสตร์ "))
+point_chemistry = int(input("คะแนนวิชาเคมี "))
+point_science = int(input("คะแนนวิชาวิทยาศาสตร์ "))
+
+total_point = (point_science + point_chemistry + point_math)
+average_point = total_point / 3
+
+print("\nคะแนนรวม:" , total_point)
+print("คะแนนเฉลี่ย : ", average_point)
+
+if average_point >= 60:
+    print("ระดับคะแนน : ควรปรับปรุง ")
+elif average_point >= 70:
+    print("ระดับคะแนน : ผ่าน ")
+else:
+    print("ระดับคะแนน : ยอดเยี่ยม ")  
+
+print("Programmer : papada rungarunchai ")    
