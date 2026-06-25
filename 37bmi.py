163a3c140404b789892fb1b6d1701d7139a573a9
papada67 <27087@bbw.ac.th>
2026-06-25 14:26:35 +0700

    สอบเก็บคะแนน
    โจทย์ข้อที่ 3  BMI

---
+++ 37bmi.py
@@ -0,0 +1,16 @@
+print("คำนวณผลBMI")
+
+bmi_weight = float(input("น้ำหนักของคุณ : "))
+bmi_height = float(input("ส่วนสูงของคุณ : "))
+
+totei_bmi =  bmi_weight / ((bmi_height / 100) ** 2)
+
+print("คำนวณผลbmi : " , totei_bmi)
+print("ผลbmi : " , totei_bmi)
+
+if totei_bmi < 18.5 :
+    print("ผลของคุณ : น้ำหนักน้อย ")
+elif totei_bmi <= 18.5-22.9 :
+    print("ผลของคุณ : ปกติ ")
+else :
+    print("ผลของคุณ : อ้วน ")
