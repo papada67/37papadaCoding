dfde61ba4ace96d93bf7961ca6a5a7f8a92d1138
papada67 <27087@bbw.ac.th>
2026-07-06 09:05:01 +0700

    เขียนโปรแกรมแม่สูตรคูณ
    ได้ความรู้ในการใช้ for loop ใช้ range <3

--- 37.py
+++ 37.py
@@ -1,20 +1,5 @@
-print("โปรแกรมคำนวณคะแนนรวม\n")
+n = int(input("แม่สูตรคูณ:  "))
 
-point_math = int(input("คะแนนวิชาคณิตศาสตร์ "))
-point_chemistry = int(input("คะแนนวิชาเคมี "))
-point_science = int(input("คะแนนวิชาวิทยาศาสตร์ "))
-
-total_point = (point_science + point_chemistry + point_math)
-average_point = total_point / 3
-
-print("\nคะแนนรวม:" , total_point)
-print("คะแนนเฉลี่ย : ", average_point)
-
-if average_point >= 60:
-    print("ระดับคะแนน : ควรปรับปรุง ")
-elif average_point >= 70:
-    print("ระดับคะแนน : ผ่าน ")
-else:
-    print("ระดับคะแนน : ยอดเยี่ยม ")  
-
-print("Programmer : papada rungarunchai ")    
+for i in range (1, 13) :
+    ans = n * i
+    print(n, "x",i, "=",n*i)
